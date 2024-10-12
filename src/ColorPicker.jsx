57f00dbf0890a5ab41144ffcd8fac651b97e9d6a
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-12 20:56:56 +0600

    update

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,26 +1,23 @@
-import React, {useState} from "react"
-function ColorPicker(){
+import React, { useState } from "react"
+function ColorPicker() {
     const [color, setColor] = useState("#FFFFFF");
 
-    function handleColorChange(e){
+    function handleColorChange(e) {
         setColor(e.target.value);
     }
 
-    function copyToClipboard(){
-        navigator.clipboard.writeText(color).then(()=>{
-            // alert("color code coppied to ")
-        }).catch(err => {
-            console.error(err)
-        })
+    function copyToClipboard() {
+        navigator.clipboard.writeText(color)
     }
-    return(
+    return (
         <div className="color-picker-container">
             <h1>Color Picker</h1>
-            <div className="color-display" style={{backgroundColor: color, cursor: "pointer"}} onClick={copyToClipboard}>
+            <div className="color-display" style={{ backgroundColor: color, cursor: "pointer" }} onClick={copyToClipboard}>
+                <p>Click to copy</p>
                 <p>{color}</p>
             </div>
             <label htmlFor="">Select a Color</label>
-            <input type="color" value={color} onChange={handleColorChange}/>
+            <input type="color" value={color} onChange={handleColorChange} />
         </div>
     )
 }
